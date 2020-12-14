47494e42fa29cb7b7b86b69c6d78bfcfedbed614
vosxodd <73089079+vosxodd@users.noreply.github.com>
2020-12-14 06:19:46 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -10,13 +10,13 @@ for i in range (1,years+1):
     print("-----------------------------------------------")
     for j in range(1, 13):
         if i == 1 and j == 1:
-            print("|    ", j, "    | ", capital, "    |   ", "%.2f"%(capital*percent/100), "   | ",  capital + float("%.2f"%(capital*percent/100)), " | ")
+            print("|    ", j, "    | ", "%.2f"%(capital), "    |   ", "%.2f"%(capital*percent/100), "   | ", "%.2f"%(capital + capital*percent/100), " |")
         elif 1<=j<=9:
             capital += investment + float("%.2f"%(capital*percent/100))
-            print("|    ", j, "    | ", (capital), "    |   ", "%.2f"%(capital*percent/100), "   | ", capital + float("%.2f"%(capital*percent/100)), "  |")
+            print("|    ", j, "    | ", "%.2f"%(capital), "    |   ", "%.2f"%(capital*percent/100), "   | ", "%.2f"%(capital + capital*percent/100), "  |")
         elif j>9:
-            capital += investment + float("%.2f"%(capital*percent/100))
-            print("|   ", j, "    | ", (capital), "    |   ", "%.2f"%(capital*percent/100), "   | ", capital + float("%.2f"%(capital*percent/100)), "  |")
+            capital += investment + capital*percent/100
+            print("|   ", j, "    | ", "%.2f"%(capital), "    |   ", "%.2f"%(capital*percent/100), "   | ", "%.2f"%(capital + capital*percent/100), "  |")
 
 
 
